26c1df85d2c1798b0ae60580869b85de1fc0cccf
LstChanceMadrid <michael.madrid@live.com>
2018-09-27 15:26:10 -0500

    updates to format

--- pool_table_management.py
+++ pool_table_management.py
@@ -1,18 +1,19 @@
 #adds ability to read and write a json file
 import json
-import time
+import datetime
 
 occupied = "Occupied"
 not_occupied = "Not Occupied"
 pool_table_number = ""
 
+
 # Table class with properties
 class PoolTable:
-    def __init__(self, pool_table_number, start_date_time = 0, end_date_time = 0):
+    def __init__(self, pool_table_number, start_date_time = "0", end_date_time = "0"):
         self.pool_table_number = pool_table_number
         self.start_date_time = start_date_time
         self.end_date_time = end_date_time
-        self.total_time_played = self.end_date_time - self.start_date_time
+        self.total_time_played = int(self.end_date_time) - int(self.start_date_time)
     
     # Turns the classes into a dictionary
     def as_dictionary(self):
@@ -24,24 +25,26 @@ class PoolTable:
 
 
     # Returns and prints the table and if it is occupied or not based on start date time
-    def occupancy(self, start_date_time = 0):
-        occupancy = ""
-        if (start_date_time == 0):
-            print(f"Table {self.pool_table_number} - {not_occupied}")
-            return not_occupied
 
-        else:
-            print(f"Table {self.pool_table_number} - {occupied}")
-            return occupied
+          
+            
 
 
-#prints the current status of all the pool tables at one time
+
 def view_table_statuses(all_pool_tables):
+
     with open("pool_table_data.json", "r") as json_object:
+        pool_table_number = json.loads(json_object.read())
 
     #print occupancy of each table
-        for pool_table in all_pool_tables:
-            pool_table.occupancy()
+        for pool_table in pool_table_number:
+
+            if (pool_table["start_date_time"] == "0"):
+                print(f"Table {pool_table['pool_table_number']} - {not_occupied}")
+
+            else:
+                print(f"Table {pool_table['pool_table_number']} - {occupied}")
+
 
 
 
@@ -68,19 +71,18 @@ def new_game():
     print("Would you like to start a new game?")
     user_input = input("Enter 'y' for yes or 'n' for no: ")
 
-    #rewrites all of the data in the json file with new values
+   
     if (user_input == 'y'):
-        with open("pool_table_data.json", "w") as json_object:
-            data=[]
-            for pool_table in all_pool_tables:
-                data.append(pool_table.as_dictionary())
-            json.dump(data,json_object)
+        print("")
+        view_table_statuses(all_pool_tables)
+        print("")
+        
         choose_table()
 
 
     elif (user_input == 'n'):
             #confirms if a user doesnt want to start a new game
-            print("Are you sure you don't want to start a new game?")
+            print("Are you sure you don't want to rent this table?")
             user_input_check = input("Enter 'y' for yes or 'n' for no: ")
 
             if (user_input_check == 'n'):
@@ -88,46 +90,95 @@ def new_game():
             
             elif (user_input_check == 'y'):
                 print("thank you for coming")
+                print("")
+                view_table_statuses(all_pool_tables)
+                new_game()
 
     # restarts the menu if an invalid option is entered        
     else:
         print("That was an invalid option.")
+        print("")
         new_game()
+
 #takes in a user input to choose the table number.
 def choose_table():
-    table_selection = int(input("Enter the table number: "))
+    print("")
+    table_selection = int(input("Enter an available table number: "))
+    
 
+# NOT USEDturn the dictionaries back to an object
     with open("pool_table_data.json", "r") as json_object:
-        pool_table_number = json.loads(json_object.read())
-        print(pool_table_number[0]["pool_table_number"])
-
+        pool_table_number = json.load(json_object)
+        #NOT USEDfor pool_table in pool_table_number
+        #NOT USEDname_of_the_table = pool_table["name"]
+        #NOT USEDsame_pool_table = PoolTable(name_of_the_table)
+        print(pool_table_number[table_selection - 1]["pool_table_number"])
+        occupancy = pool_table_number[table_selection - 1]["start_date_time"]
         # withdraw the specific tables data occupancy
-        if (table_selection == pool_table_number[table_selection - 1]["pool_table_number"]):
-            occupancy = all_pool_tables[table_selection - 1].occupancy()
+        if (occupancy != "0"):
+            
+            print("This table is occupied")
             print(occupancy)
+            choose_table()
 
-            if (occupancy == occupied):
-                print("Table is occupied. choose a new table")
-                choose_table()
                 
-            if (occupancy == not_occupied):
+        if (occupancy == "0"):
+            rent = input("Would you like to rent this table? 'y'/'n': ")
+            if (rent == 'y'):
+                pool_table_number[table_selection - 1]["start_date_time"] = datetime.datetime.now().time().strftime('%H:%M:%S')
+                start_time = pool_table_number[table_selection - 1]["start_date_time"]
+                print("good to go")
+                print(start_time)
+                print("")
+
+   
+                with open("pool_table_data.json", "w") as json_object:
+                    json.dump(pool_table_number, json_object)
+            elif (rent == 'n'):
+
+                choose_table()
+            else:
+                print("That was an invalid option.")
+                choose_table()
+            
+
+    
+            
+
+    
+                    
+                
+                
+
                 #start the start time date
                 #save that data into the json
-                print("This table is free")
-
-        else:
-            print("its not working")
-
+        print("You have now rented this table!")
+        print(f"Your start time is ")
+        new_game()
 
 
 
 
+
+
+
+
+print("")
 
 print("Pool Table Manager Active")
 
+print("")
+
 print(" - Table Statuses")
 
+print("")
 
 view_table_statuses(all_pool_tables)
+
+print("")
+
+print(f"Current time is - {datetime.datetime.now().hour}:{datetime.datetime.now().minute}")
+
+print("")
 
 new_game()
